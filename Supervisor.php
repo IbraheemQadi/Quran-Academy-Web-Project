33d6b9912db6e81ddf502593241ddb525924396c
Ibraheem Qadi <ibra444hem@gmail.com>
2023-05-12 11:08:10 +0300

    Add Datepicker for updating student.

--- Supervisor.php
+++ Supervisor.php
@@ -337,21 +337,23 @@ if(isset($_SESSION['isSupervis'])){
                 >
               </div>
               <!--BD input-->
-              <div class="relative mb-12" data-te-input-wrapper-init>
-                <input
-                  type="text"
-                  name="BD"
-                  class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                  autocomplete="off"
-                />
-                <label
-                  for="exampleInputEmail1"
-                  class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                  >تاريخ الميلاد</label
-                >
-              </div>
+              <div
+              class="relative mb-12"
+              data-te-datepicker-init
+              data-te-format="yyyy-mm-dd"
+              data-te-input-wrapper-init
+            >
+              <input
+                type="text"
+                name="BD"
+                class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+              />
+              <label
+                for="floatingInput"
+                class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                >تاريخ الميلاد</label
+              >
+            </div>
               <!--Address input-->
               <div class="relative mb-12" data-te-input-wrapper-init>
                 <input
@@ -726,7 +728,7 @@ for ( $i = 0 ; $i < $es->num_rows; $i++ ) {
          <td>$rw[NameSoraREV]</td>
          <td>$rw[Rev_for]</td>
          <td>$rw[Rev_grad]</td>
-         <td ><a onclick='addToStorage($rw[ST_ID]); addIndixToStorage($rw[INDIX]);' type='button' data-te-toggle='modal' data-te-target='#deleteReportModal' data-te-ripple-init ><ion-icon name='trash' size='large' ></ion-icon></a>
+         <td ><a onclick='addToStorage($rw[ST_ID]);addIndixToStorage($rw[INDIX]);' type='button' data-te-toggle='modal' data-te-target='#deleteReportModal' data-te-ripple-init ><ion-icon name='trash' size='large' ></ion-icon></a>
          <a onclick='addToStorage($rw[ST_ID]); addIndixToStorage($rw[INDIX]); setModalData($rw[ST_ID]);' type='button' data-te-toggle='modal' data-te-target='#updateStudentModal' data-te-ripple-init ><ion-icon name='create' size='large' ></ion-icon></a></td>
      </tr>";
 
--- utils/deleteReport.php
+++ utils/deleteReport.php
@@ -6,7 +6,6 @@ if ( isset( $_GET[ 'stid' ] ) && isset( $_GET[ 'INDIX' ] ) ) {
     $index = $_GET[ 'INDIX' ];
 
     $db = new mysqli( 'localhost', 'root', '', 'academy' );
-    // $query = "Delete FROM students where ST_ID = $studentId AND INDIX = $index ";
     $query = "DELETE FROM report WHERE `report`.`ST_ID` = $studentId AND `report`.`INDIX` = $index ";
     $db->query( $query );
     $db->commit();
